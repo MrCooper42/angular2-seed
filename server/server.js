967d796c8c5236321aa58e98c841702898dc6a3f
MrCooper42 <mr.cooper42co@gmail.com>
2016-12-15 09:16:22 -0700

    seed working, do auth

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({
 	extended: false
 }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
